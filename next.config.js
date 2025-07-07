f0d1d24285dea001c97c1c8c0e4e34cf74a9a75b
Bradymck <mckmuze@gmail.com>
2025-07-06 22:45:10 -0700

    refactor: simplify Next.js configuration and startup scripts

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: {
     unoptimized: true
-  },
-  experimental: {
-    outputFileTracingRoot: process.cwd(),
   }
 }
 
